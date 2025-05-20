deeaab9ae1fea69bc36d7b77a6015b55ace73078
Jonas Otten <jonas.otten.00@gmail.com>
2025-05-20 12:50:47 +0200

    Fix: Missing training metrics, broken font size

--- utils/plots.py
+++ utils/plots.py
@@ -85,7 +85,7 @@ class Annotator:
         if self.pil or not is_ascii(label):
             self.draw.rectangle(box, width=self.lw, outline=color)  # box
             if label:
-                w, h = self.font.getsize(label)  # text width, height
+                w, h = self.get_font_size()
                 outside = box[1] - h >= 0  # label fits outside box
                 self.draw.rectangle(
                     (box[0], box[1] - h if outside else box[1], box[0] + w + 1,
@@ -117,12 +117,18 @@ class Annotator:
 
     def text(self, xy, text, txt_color=(255, 255, 255)):
         # Add text to image (PIL-only)
-        w, h = self.font.getsize(text)  # text width, height
+        _, h = self.get_font_size()
         self.draw.text((xy[0], xy[1] - h + 1), text, fill=txt_color, font=self.font)
 
     def result(self):
         # Return annotated image as array
         return np.asarray(self.im)
+
+    def get_font_size(self):
+        bbox = self.draw.textbbox((0, 0), "Hello, world!", font=self.font)
+        width = bbox[2] - bbox[0]
+        height = bbox[3] - bbox[1]
+        return width, height
 
 
 def feature_visualization(x, module_type, stage, n=32, save_dir=Path('runs/detect/exp')):
